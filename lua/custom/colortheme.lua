634cc69f33ab55a22f86a0312a375a813e8d8f7a
ki-ba <barru.killian@pm.me>
2025-11-01 17:16:45 +0100

    fix lazy unable to install/update catpuccin

--- lua/custom/colortheme.lua
+++ lua/custom/colortheme.lua
@@ -4,7 +4,7 @@ return {
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'catpuccin/nvim',
+  'https://github.com/catppuccin/nvim.git',
   name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
